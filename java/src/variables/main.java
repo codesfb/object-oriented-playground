a1dd2a5ebfe42a86b6f5397a77ce19169c40bf33
codesfb <fabio.santana950@gmail.com>
2026-02-07 14:00:08 -0300

    first commit

---
+++ java/src/variables/main.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] args){
+        // variables
+
+        //declaration
+        int age;
+
+        // assignmet
+
+        age = 26;
+        System.out.println(age);
+
+
+    }
+
+}
