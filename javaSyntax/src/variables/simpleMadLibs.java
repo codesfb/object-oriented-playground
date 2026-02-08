a832d0a487f093a55cf2b3b23d16a28454ccd008
codesfb <fabio.santana950@gmail.com>
2026-02-08 11:34:58 -0300

    update

--- javaSyntax/src/variables/simpleMadLibs.java
+++ javaSyntax/src/variables/simpleMadLibs.java
@@ -13,7 +13,7 @@ public class simpleMadLibs {
 
 
 
-        String phrase = "The " + adjective + "student " + verb + "to the " + noun;
+        String phrase = "The " + adjective + " student " + verb + " to the " + noun;
         System.out.println(phrase);
         scanner.close();
     }
