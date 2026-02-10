c8c19f9c175758f5838daf704387733decdb6ef6
codesfb <fabio.santana950@gmail.com>
2026-02-10 15:38:21 -0300

    update

---
+++ java/src/javaSyntax/AritmeticOperators.java
@@ -0,0 +1,28 @@
+public class AritmeticOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Values of a and b: " + a + ", " + b);
+
+        // Addition
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+
+        // Subtraction
+        int difference = a - b;
+        System.out.println("Difference: " + difference);
+
+        // Multiplication
+        int product = a * b;
+        System.out.println("Product: " + product);
+
+        // Division
+        int quotient = a / b;
+        System.out.println("Quotient: " + quotient);
+
+        // Modulus
+        int remainder = a % b;
+        System.out.println("Remainder: " + remainder);
+    }
+}
