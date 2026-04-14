38f20d11d1d7bb7e8acd16981f808c83c8726891
codesfb <fabio.santana950@gmail.com>
2026-04-14 15:32:55 -0300

    update

--- src/main/java/Figure/MainFigure.java
+++ src/main/java/Figure/MainFigure.java
@@ -1,6 +1,8 @@
 
 //package main.java.Figure;
 
+import java.util.Arrays;
+
 public class MainFigure {
     static void main() {
         Circle circle = new Circle(1, 2, 1.0);
@@ -13,9 +15,20 @@ public class MainFigure {
 
         Figure[] figuras = new Figure[150];
 
-        for (int i = 0; i < 150; i++) {
+        for (int i = 0; i < 50; i++) {
+            double valor = i + 1;
+            figuras[i] = new Circle(i+1, i+1, valor);
+            figuras[i + 50] = new Rectangle(i+1, i+1, valor, valor);
+            figuras[i + 100] = new Triangle(i+1, i+1, valor, valor, valor);
 
         }
+        double allAreas=0.0;
+        for(Figure figure : figuras){
+            allAreas+=figure.area();
+            System.out.println(figure );
+        }
+        System.out.printf("Soma de todas as areas %.2f", allAreas);
+
 
 
     }
