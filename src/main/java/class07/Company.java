8bd9964952444b7e3b70503a73090b386a12462d
codesfb <fabio.santana950@gmail.com>
2026-05-07 15:37:11 -0300

    update

---
+++ src/main/java/class07/Company.java
@@ -0,0 +1,101 @@
+package class07;
+
+//import Employee.Employee;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class Company {
+
+    static void main() {
+
+        var ada = new Employee("01", "Ada Lovelace", "Computer Programmer", 10_000.0, LocalDate.now());
+        var dijkstra = new Employee("03", "Edsger Dijkstra", "Computer Scientist", 10_400.0, LocalDate.now().minusYears(2));
+        var huffman = new Employee("02", "David Huffman", "Computer Scientist", 10_700.0, LocalDate.now().minusYears(1));
+
+
+        var comp = new Company();
+
+        if (comp.hire(ada,dijkstra, huffman)) {
+            System.out.println(" Contraado com sucesso");
+        } else {
+            System.out.println("Erro ao cotratar");
+        }
+
+        comp.getEmployees();
+        //o my days who i will fire now?
+
+        comp.fire("01");
+        System.out.println(ada.getName() +  " fired");
+
+        comp.getEmployees("Computer Scientist");
+        System.out.println("Payment");
+        System.out.println(comp.pay("01"));
+        System.out.println(comp.pay("02"));
+        System.out.println(comp.pay("03"));
+
+        System.out.println("Increase salary");
+        System.out.println("Hufman salary before " + huffman.getSalary());
+        comp.increaseSalary("02",20_000.0);
+        System.out.println("Hufman salary after " + huffman.getSalary());
+        System.out.println("Average salary " + comp.averageSalary("Computer Scientist"));
+
+
+    }
+    //TODO remake this
+    Set<Employee> company = new LinkedHashSet<>();
+
+    public boolean hire(Employee...employee){
+        return Collections.addAll(this.company, employee);
+    }
+
+    public void fire(String id){
+            company.removeIf(e -> e.getId().equals(id));
+    }
+
+    public void getEmployees(){
+        company.forEach(System.out::println);
+    }
+
+    public void getEmployees(String jobTitle){
+        company.stream()
+                .filter(e -> e.getJobTitle().equals(jobTitle))
+                .forEach(System.out::println);
+    }
+
+    public boolean pay(String id){
+        return company.stream()
+                .filter(employee -> id.equals(employee.getId()))
+                .anyMatch(employee -> employee.createPaycheck(LocalDate.now()));
+
+     }
+    public void increaseSalary(String id, double newSalary){
+        company.stream()
+                .filter(e -> id.equals(e.getId()))
+                .findFirst()
+                .ifPresent(e->
+                    e.setSalary(newSalary)
+                );
+    }
+
+    public double averageSalary(String jobTitle){
+       return company.stream()
+                .filter(e -> e.getJobTitle().equals(jobTitle))
+                .mapToDouble(Employee::getSalary)
+                .average().orElse(0.0);
+
+
+    }
+    public double averageSalary(LocalDate inicio, LocalDate fim){
+        return company.stream()
+                .filter(e -> e.getDateOfEmployment().isAfter(inicio)&&
+                        e.getDateOfEmployment().isBefore(fim) )
+                .mapToDouble(Employee::getSalary)
+                .average().orElse(0.0);
+
+
+    }
+
+
+
+}
---
+++ src/main/java/class07/Employee.java
@@ -0,0 +1,125 @@
+package class07;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.*;
+
+public  class Employee implements Comparable<class07.Employee> {
+        private String id;
+        private String name;
+        private String jobTitle;
+        private double salary;
+        private LocalDate dateOfEmployment;
+        private List<Paycheck> paychecks;
+        private Set<TechnicalSkill> skills;
+
+        public Employee() {}
+
+        public Employee(String id, String name, String jobTitle, double salary, LocalDate dateOfEmployment) {
+            this.id = id;
+            this.name = name;
+            this.jobTitle = jobTitle;
+            this.salary = salary;
+            this.dateOfEmployment = dateOfEmployment;
+            this.skills = new HashSet<>();
+            this.paychecks = new ArrayList<>();
+        }
+
+        @Override
+        public String toString() {
+            return String.format("""
+                   Employee{id='%s', name='%s', jobTitle='%s', salary=%.2f, dateOfEmployment=%s}""",
+                    id , name, jobTitle, salary, dateOfEmployment);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            class07.Employee employee = (class07.Employee) o;
+            return Objects.equals(id, employee.id);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id);
+        }
+
+        public boolean createPaycheck(LocalDate payday){
+            return paychecks.add(new Paycheck(payday, salary));
+        }
+
+        public void removePaycheck(Paycheck paycheck){
+            paychecks.remove(paycheck);
+        }
+
+        public Iterator<Paycheck> iteratorPaychecks(){
+            return paychecks.iterator();
+        }
+
+        public void addSkill(TechnicalSkill skill){
+            skills.add(skill);
+        }
+
+        public void removeSkill(TechnicalSkill skill){
+            skills.remove(skill);
+        }
+
+        public boolean hasSkill(TechnicalSkill skill){
+            return skills.contains(skill);
+        }
+
+        public Set<TechnicalSkill> getSkills(){
+            return Set.copyOf(skills);
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public LocalDate getDateOfEmployment() {
+            return dateOfEmployment;
+        }
+
+        public final int getYearsOfService() {
+            return Period.between(dateOfEmployment, LocalDate.now()).getYears();
+        }
+
+        public void setDateOfEmployment(LocalDate dateOfEmployment) {
+            this.dateOfEmployment = dateOfEmployment;
+        }
+
+        public void setJobTitle(String jobTitle) {
+            this.jobTitle = jobTitle;
+        }
+
+        public void setSalary(double salary) {
+            this.salary = salary;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getJobTitle() {
+            return jobTitle;
+        }
+
+        public double getSalary() {
+            return salary;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        @Override
+        public int compareTo(class07.Employee other) {
+            return id.compareTo(other.id);
+        }
+}
+
